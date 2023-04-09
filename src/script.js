ecb5c55f5783a6fa0cd81a3fc51ef0bf933697f3
Rene <rgombar@gmail.com>
2023-04-09 10:30:34 -0500

    finished particles and gsap animation

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
-
+import gsap from 'gsap'
 /**
  * Debug
  */
@@ -10,15 +10,19 @@ const gradient = textureLoader.load('textures/gradients/5a.jpg')
 gradient.magFilter = THREE.NearestFilter
 gradient.generateMipmaps = false
 
-
 const gui = new dat.GUI()
 
 const parameters = {
-    materialColor: '#ffeded'
+    materialColor: '#ffeded',
+    particlesColor: '#ffeded'
 }
 
 gui.addColor(parameters, 'materialColor').onChange(()=>{
     material.color.set(parameters.materialColor)
+})
+
+gui.addColor(parameters, 'particlesColor').onChange(()=>{
+    particlesMaterial.color.set(parameters.particlesColor)
 })
 
 /**
@@ -35,8 +39,10 @@ const scene = new THREE.Scene()
 /**Objects */
 const objectsDistance = 4
 
-const material = new THREE.MeshToonMaterial({color:parameters.materialColor})
-material.gradientMap = gradient
+const material = new THREE.MeshToonMaterial({
+    color:parameters.materialColor,
+    gradientMap: gradient,
+})
 
 const mesh1 = new THREE.Mesh(
     new THREE.TorusGeometry(1, 0.4 ,16, 69),
@@ -65,10 +71,35 @@ scene.add(mesh1, mesh2, mesh3)
 
 const sectionMeshes = [mesh1, mesh2, mesh3]
 
+/**Particles */
+const particleCount = 2000
+const positions = new Float32Array(particleCount*3)
+
+for (let i=0; i < particleCount; i++){
+    positions[i*3] = (Math.random() - 0.5) * 10
+    positions[i*3+1] = objectsDistance * 0.5 -  (Math.random() * objectsDistance * sectionMeshes.length)
+    positions[i*3+2] = (Math.random() - 0.5) * 10
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions,3))
+
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation:true,
+    //depthWrite:false,
+    size: 0.05
+})
+
+const particles = new THREE.Points(particlesGeometry,particlesMaterial)
+
+scene.add(particles)
+
+
 
 
 /**Light */
-const directionalLight = new THREE.DirectionalLight('#ffff00',1)
+const directionalLight = new THREE.DirectionalLight('#ffffff',1)
 directionalLight.position.set (1,1,0)
 
 scene.add(directionalLight)
@@ -123,9 +154,29 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 //get scrollY value
 let scrollY = window.scrollY
+let currentSection = 0
+
 
 window.addEventListener('scroll',()=>{
     scrollY = window.scrollY
+    const newSection = Math.round(scrollY / sizes.height)
+    //console.log(newSection)
+    if (newSection != currentSection){
+        currentSection = newSection
+        //console.log(sectionMeshes[currentSection])
+        
+        gsap.to(
+            sectionMeshes[currentSection].rotation,
+            {
+                duration:1.5,
+                ease: 'power2.inOut',
+                x: '+=2',
+                y: '+=3',
+                z: '+= 1.5'
+
+            }
+        )
+    }
 })
 
 //Add parallax by reading the cursor position
@@ -156,8 +207,8 @@ const tick = () =>
 
     //Animate meshes
     for (const mesh of sectionMeshes){
-        mesh.rotation.x = elapsedTime * 0.1
-        mesh.rotation.y = elapsedTime * 0.08
+        mesh.rotation.x += deltaTime * 0.1
+        mesh.rotation.y += deltaTime * 0.08
     }
 
     //Animate camera based on scrollY value
